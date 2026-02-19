168fb0f79fa0f12a0bd6996369514d7fb8ad05a7
Pawan Nagar <pawannagar2243@gmail.com>
2026-02-19 10:51:32 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: "https://google-2u0v.onrender.com",
+  origin: "https://google-2-s99c.onrender.com",
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
